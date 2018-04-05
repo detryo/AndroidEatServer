3c64423718f6f96e886ea785ccedd0dee0cb5d0b
Christian <chris.sedano.arenas@gmail.com>
2018-04-05 14:58:13 +0100

    Load all List menu from Firebase to Service Side
    Create Interface, Category Model, add_new_menu_layout, menu_item, add
    library FirebaseUI, implement code in Home.java and MainActivity.java

---
+++ app/src/main/java/cristian_sedano/androideatserver/Interface/ItemClickListener.java
@@ -0,0 +1,12 @@
+package cristian_sedano.androideatserver.Interface;
+
+import android.view.View;
+
+/**
+ * Created by Christian on 01/04/2018.
+ */
+
+public interface ItemClickListener {
+
+    void onClick(View view, int position, boolean isLongClick);
+}
